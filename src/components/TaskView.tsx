468e6305e2aad077ebfe4e635e54a2d1ed1e2aa1
Martin Mehlitz <ext.martin.mehlitz@nordlb.de>
2025-10-06 14:54:14 +0200

    make the games full size

--- src/components/TaskView.tsx
+++ src/components/TaskView.tsx
@@ -1,4 +1,4 @@
-import Image from "next/image";
+"use client";
 import { Task } from "@/lib/contentful/apis/tasks-api";
 import ClickAreaView from "./ClickAreaView";
 import TrueFalseGameView from "./TrueFalseGameView";
@@ -10,38 +10,35 @@ type TaskViewProps = {
 };
 
 export default function TaskView({ task, preview }: TaskViewProps) {
-
-
+  
   return (
     <AudioProvider>
-      
-     <div
-      className="mx-auto block"
-      style={{
-        position: "relative",
-        width: task.imageWidth ?? 893,
-        height: task.imageHeight ?? 500,
-      }}
-    >
-      {task.image?.url && (
-        <Image
-          src={task.image.url ?? ""}
-          alt={task.name}
-          width={task.imageWidth ?? 893}
-          height={task.imageHeight ?? 500}
-          style={{ display: "block" }}
-        />
-      )}
-
-      {task.simpleInteractions.map((area) => (
-        <ClickAreaView key={area.id} clickArea={area} preview={preview} />
-      ))}
-      
-      {task.trueFalseGames.map((game) => (
-        <TrueFalseGameView key={game.id} game={game} preview={preview} />
-      ))}
-    </div>
-
+      <div className="w-full h-full flex items-center justify-center bg-white">
+        <div
+          className="relative w-full "
+          style={{
+            backgroundImage: `url(${task.image?.url})`,
+            backgroundSize: "contain",
+            backgroundRepeat: "no-repeat",
+            backgroundPosition: "center",
+            aspectRatio: `${task.imageWidth ?? 893} / ${task.imageHeight ?? 500}`,
+          }}
+        >
+          {/* Overlay interactive areas */}
+          {task.simpleInteractions.map((area) => (
+            <ClickAreaView
+              key={area.id}
+              clickArea={area}
+              preview={preview}
+              imageWidth={task.imageWidth ?? 893}
+              imageHeight={task.imageHeight ?? 500}
+            />
+          ))}
+          {task.trueFalseGames.map((game) => (
+            <TrueFalseGameView key={game.id} game={game} preview={preview} />
+          ))}
+        </div>
+      </div>
     </AudioProvider>
   );
 }
